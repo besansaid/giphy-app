3b914e6dfb7030e6c8ea60cf1b68abf6f54620b0
besansaid <besanhosnysaid203998@gmail.com>
2022-07-07 09:20:36 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import TextList from "./components/TextList";
+import Error from "./components/Error";
+import { GiphyFetch } from "@giphy/js-fetch-api";
+import { useState } from "react";
+import "./App.css";
+
+const giphy = new GiphyFetch("zrSMDVfLzxjATPQVJhY4LcF3gPkWuVul");
 
 function App() {
+  const [text, setText] = useState("");
+  const [results, setResults] = useState([]);
+  const [err, setErr] = useState(false);
+
+  const handleInput = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    if (text.length === 0) {
+      //set error state to true
+      setErr(true);
+      return;
+    }
+
+    console.log(text);
+
+    console.log(giphy, 5555555555);
+    const apiCall = async () => {
+      const res = await giphy.animate(text, { limit: 20 });
+
+      setResults(res.data);
+    };
+
+    apiCall();
+    //change error state back to false
+    setText("");
+    setErr(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Animated Text Generator</h1>
+      <h3>Type text into the form and hit submit</h3>
+      <input className="input-field" value={text} onChange={handleInput} />
+      <button className="submit-btn" onClick={handleSubmit}>
+        Submit
+      </button>
+      <Error isError={err} text="need length longer than 0 for input" />
+      {results && <TextList gifs={results} />}
     </div>
   );
 }
